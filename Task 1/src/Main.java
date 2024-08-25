a8c9f3a592b86b3836de61bfa16697160884d2d5
BorisRojnov <BorisRojnov@yandex.by>
2024-08-25 14:35:47 +0300

    Homework complete

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+    Phone nokia = new Phone("+375298830933", "nokia", 3.8);
+        System.out.println("Номер телефона " + nokia.phoneNumber + " модель телефона " + nokia.phoneModel
+        + " вес телефона " + nokia.phoneWeight + " кг " + " производитель " + Phone.phoneCountryOrigin);
 
+    Phone samsung = new Phone("+375336547126", "samsung", 3);
+        System.out.println("Номер телефона " + samsung.phoneNumber + " модель телефона " + samsung.phoneModel
+                + " вес телефона " + samsung.phoneWeight + " кг " + " производитель " + Phone.phoneCountryOrigin);
+
+       Phone.receiveCall("Ольга");
+       Phone.receiveCall("Borya", "+375115554039");
     }
+
+
 }
